e0f72ba7d71fa2dc4dfb03ac3570e3fbbf8b8b51
jimoh oluwaseyi <heysey128@gmail.com>
2023-11-02 16:03:46 +0100

    Create api to the data and retrieve it

--- src/modules/displayScores.js
+++ src/modules/displayScores.js
@@ -1,41 +1,9 @@
-const scoreList = [{
-  name: 'Jim',
-  score: 10,
-},
-{
-  name: 'Smooth',
-  score: 15,
-},
-{
-  name: 'Subby',
-  score: 20,
-},
-{
-  name: 'Gift',
-  score: 25,
-},
-{
-  name: 'Repeat',
-  score: 30,
-},
-{
-  name: 'Repeat',
-  score: 30,
-},
-{
-  name: 'Repeat',
-  score: 30,
-},
-{
-  name: 'Repeat',
-  score: 30,
-},
-];
-const displayScore = () => {
+const displayScore = (scoreList) => {
   const scores = document.querySelector('.scores');
+  scores.innerHTML = '';
   scoreList.forEach((score) => {
     const list = document.createElement('li');
-    list.innerHTML = `${score.name}: ${score.score}`;
+    list.innerHTML = `${score.user}: ${score.score}`;
     scores.appendChild(list);
   });
 };
